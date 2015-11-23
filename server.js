6583295f770fedea492d12f51c444bba5a5dfc2a
Sung Won Hwang <sunghw@gmail.com>
2015-11-23 02:24:25 -0500

    socketio added

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ var app = express();
 
 //built in node module
 var http = require('http').Server(app);
+
+var io = require('socket.io')(http);
+
+
 //start a new server and use this express app as a boiler plate
 app.use(express.static(__dirname + '/public'));
 
+io.on('connection', function(){
+	console.log('User connected via socket.io');
+})
+
 http.listen(PORT, function(){
 	console.log('server started at port '+ PORT);
-	
+
 })
