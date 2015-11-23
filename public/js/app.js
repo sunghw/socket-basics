1d847b042d443d315d5dc048046dd907650c5c29
Sung Won Hwang <sunghw@gmail.com>
2015-11-23 08:37:19 -0500

    latest

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,7 @@ var socket = io(); //defined when we load in socketio libary. goes to io.on(conn
 
 socket.on('connect', function(){
 	console.log('Connected to socketio server!');
-})
+});
+socket.on('message', function (message) {
+	console.log('Got the mssage from the server: ' + message.text);
+});
--- server.js
+++ server.js
@@ -11,9 +11,21 @@ var io = require('socket.io')(http);
 //start a new server and use this express app as a boiler plate
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(){
+io.on('connection', function (socket){
 	console.log('User connected via socket.io');
+	
+	socket.on ('message', function (message){
+		console.log('Message received from client' + message.text);
+		//socket.broadcast.emit
+		//io.emit('message', message);
+		socket.broadcast.emit('message', message);
+	});
+
+	socket.emit ('message', {
+		text: "Welcome to the chat application!"
+	});
 })
+
 
 http.listen(PORT, function(){
 	console.log('server started at port '+ PORT);
